878971e8fd630d7a283dea1b37e2b19733fe7953
Jacob Harrison Lyon <jacobhlyon@Jacobs-MacBook-Air.local>
2017-07-10 08:54:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+	attr_accessor :word_to_match, :new_word
+
+	def initialize(word_to_match)
+		@word_to_match = word_to_match.chars.sort.join
+	end
+
+	def match(possible_words)
+		correct_words = []
+		possible_words.each do |word|
+			@new_word = word.chars.sort.join
+			if @new_word == @word_to_match
+				correct_words << word
+			end
+		end
+		return correct_words
+	end
+
+end
